0b974549816430ac2d14f8e07479106325115387
Estelle <estelle.gresillon@kardinal.ai>
2020-10-23 18:53:05 +0200

    started challenge 3

---
+++ src/components/FancySidebar/FancySidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./FancySidebar.scss";
+
+const FancySidebar = () => {
+  return (
+    <div id="Fancy-Sidebar">
+      <nav>
+        <ul>
+          <li>hi</li>
+          <li>hi</li>
+          <li>hi</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default FancySidebar;
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import StripeMenu from "components/StripeMenu";
 import PricingComparator from "components/PricingComparator";
+import FancySidebar from "components/FancySidebar";
 
 export const FRONT_END_CHALLENGES = [
   { name: "Stripe Menu", link: "/stripe-menu", component: StripeMenu },
@@ -7,5 +8,10 @@ export const FRONT_END_CHALLENGES = [
     name: "Pricing Comparator",
     link: "/pricing-comparator",
     component: PricingComparator,
+  },
+  {
+    name: "Fancy Sidebar",
+    link: "/fancy-sidebar",
+    component: FancySidebar,
   },
 ];
