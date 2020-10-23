a7b7e95931a9c6eb573832e073b5ac719e16de7e
Estelle <estelle.gresillon@kardinal.ai>
2020-10-24 00:08:11 +0200

    started challenge 4

--- src/components/CreditCardForm/CreditCardForm.js
+++ src/components/CreditCardForm/CreditCardForm.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import times from "lodash/times";
 // import { Link } from "react-router-dom";
 
 import "./CreditCardForm.scss";
 
 const CreditCardForm = () => {
+  const [isCardReversed, setIsCardReversed] = useState(false);
+  const [CCV, setCCV] = useState("");
+
+  const handleCCVChange = (value) => {
+    if (value.length > 3) {
+      return;
+    }
+    setCCV(value);
+  };
+
   return (
     <div id="Credit-Card-Form">
-      <div className="form-container">hi</div>
+      <div className="form-container">
+        <div className="credit-card-container">
+          <div
+            className="credit-card-front"
+            style={{ transform: isCardReversed ? "rotateY(180deg)" : "none" }}
+          >
+            front
+          </div>
+          <div
+            className="credit-card-back"
+            style={{ transform: isCardReversed ? "none" : "rotateY(180deg)" }}
+          >
+            {times(CCV.length, () => "*")}
+          </div>
+        </div>
+        <input
+          type="number"
+          className="ccv-input"
+          value={CCV}
+          onChange={(e) => handleCCVChange(e.target.value)}
+          onFocus={() => setIsCardReversed(true)}
+          onBlur={() => setIsCardReversed(false)}
+        />
+      </div>
     </div>
   );
 };
